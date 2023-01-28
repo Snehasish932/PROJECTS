419934d46c6de0223667912a086a18d75965545e
Snehasish932 <122681550+Snehasish932@users.noreply.github.com>
2023-01-28 20:02:58 +0530

    Add files via upload

---
+++ portfolio-project-main/robo.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main{}
+{
+    int t ;
+cin>>t;
+if(t>10){cout<<"u are right"<<endl}
+    
+
+else{
+    cout<<"input is greter than t"<<endl
+}
+
+
+}
